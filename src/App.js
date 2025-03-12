c6905112636ba9640371bd0fa023e4b79098b9f6
kjh311 <kjh311@gmail.com>
2025-03-11 19:45:41 -0600

    useState

--- src/App.js
+++ src/App.js
@@ -3,62 +3,18 @@ import "./App.css";
 import React from "react";
 import ImageGallery from "./ImageGallery";
 import Counter from "./Counter";
-
-const nums = [1, 2, 3, 4, 5, 6];
-
-const person = {
-  name: "David",
-  age: 45,
-  occupation: "Government employee",
-};
-
-const people = {
-  peeps: [
-    { name: "Kevin", age: 46 },
-    { name: "David", age: 45 },
-    { name: "Eric", age: 39 },
-    { name: "Peter", age: 45 },
-  ],
-};
-
-const totalPeopleAge = people.peeps.reduce((sum, peep) => sum + peep.age, 0);
-console.log(totalPeopleAge);
-
-const peepNames = people.peeps.map((peep) => peep.name);
-console.log(peepNames);
-
-const { name, age, occupation } = person;
-const [one, two, three, four, five, six] = nums;
-
-const numbers = [...nums, 7, 8];
-
-const totalNumbers = numbers.reduce((sum, num) => sum + num, 0);
-console.log(totalNumbers);
+import Test from "./Test";
+import NameForm from "./NameForm";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      {nums.map((num) => (
-        <div key={num}>
-          {num > 2 ? (
-            <span className="blue">{num}</span>
-          ) : (
-            <span className="red">{num}</span>
-          )}
-        </div>
-      ))}
-
-      {Object.entries(person).map(([key, value]) => (
-        <div key={key}>
-          <strong>{key}:</strong> {value}
-        </div>
-      ))}
-
-      <div>{numbers.join(", ")}</div>
+      <h1>React Practice</h1>
+      {/* <Test /> */}
 
       {/* <ImageGallery /> */}
-      <Counter />
+      {/* <Counter /> */}
+      <NameForm />
     </div>
   );
 }
---
+++ src/NameForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function NameForm() {
+  const [name, setName] = useState("");
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Hello, ${name}!!!!`);
+    setName("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={handleChange} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default NameForm;
